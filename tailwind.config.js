90e13efe02621511199eedade417feddd50c03e3
saswat mishra <saswatmishra.official@gmail.com>
2022-01-24 07:28:30 +0000

    customize_tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    screens: {
+      xsm: '500px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    // See to customize breakpoints we needed put all the breakpoints and directly under screens.
+    extend: {
+      spacing:{
+        13: "3.2rem"
+      },
+      fontSize:{
+        '10xl': ['10rem', {lineHeight: '1.2'}]
+      }
+    },
   },
   plugins: [],
 }
